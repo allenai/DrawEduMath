d1533ea87a4598e1ba29ea66ad27c72ba58fe235
Nathan Anderson <101899832+nathan-anderson16@users.noreply.github.com>
2025-07-27 16:37:06 -0400

    Add scripts for running models on Alibaba, Google Cloud, and VLLM. Also add a script from Sami for TogetherAI.

---
+++ scripts/vlm_generations/text_to_text_generation.py
@@ -0,0 +1,47 @@
+"""
+
+Script to generate texts from a given text using the Together api and Mixtral 7X22B model.
+
+@Sami Baral
+
+"""
+
+from together import Together
+from dotenv import dotenv_values
+from data.caption_prompt import prompt_v1
+
+
+class TogetherTextToText:
+    def __init__(self):
+        config = dotenv_values("../../.env")
+        api_key = config.get("TOGETHER_API_KEY")
+        self.model = "mistralai/Mixtral-8x22B-Instruct-v0.1"
+        self.client = Together(api_key=api_key)
+
+    def get_response(self, system_prompt, user_prompt):
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+        )
+
+        return response.choices[0].message.content
+
+
+if __name__ == "__main__":
+    together_text = TogetherTextToText()
+
+    system_prompt = prompt_v1.DECOMPOSE_PROMPT
+    caption = """
+    This is a digital, typed and handwritten image. There is a graph. One point on the graph is shaded in with a circe. 
+    The student has drawn a downward-sloping line through the point. The student has drawn a slope triangle beneath the 
+    line. The vertical part of the slope triangle has a label of -12. The horizontal part of the slope triangle has a 
+    label of 6. This shows the line has a slope of -2.
+    """
+    user_prompt = f"Decompose this caption: {caption}"
+
+    response = together_text.get_response(system_prompt, user_prompt)
+    print("Output: ")
+    print(response)
